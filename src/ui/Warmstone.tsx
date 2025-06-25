cbe2b91078395d7d93cf4a4b1b48c7cc1ed605aa
juliagalaton <iulia.galaton@gmail.com>
2025-06-25 16:29:14 +0200

    Added % to the progressbar

--- src/ui/Warmstone.tsx
+++ src/ui/Warmstone.tsx
@@ -9,8 +9,8 @@ export default function Hearhstone() {
             <p className="text-sm text-gray-400 mt-1">Keep the stone's heart aglow!</p>
             {(vitality < 50) && <p className="text-green-400 italic h-6 my-2">Darkness gathers as the stone fades!</p>}
            <div className="pt-6">
-            <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                <div className="bg-purple-500  h-2.5 rounded-full" style={{ width: vitality.toString().concat("%") }}></div>
+            <div className="w-full bg-gray-200 rounded-full h-3.5 dark:bg-gray-700">
+                <div className="bg-purple-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: vitality.toString().concat("%") }}>{vitality}%</div>
             </div>
             </div>
         </div >
